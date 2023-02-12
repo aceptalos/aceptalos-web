20a9bbe383eea346be882750cc98f9d7da7192b4
SrCroqueta <32432079+SrCroqueta@users.noreply.github.com>
2023-02-12 16:20:26 +0100

    Add files via upload

---
+++ assets/js/hideorshow.button.scrollup.js
@@ -0,0 +1,13 @@
+﻿// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 750px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
