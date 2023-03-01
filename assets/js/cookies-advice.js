27d3d8ecce17d02aa34016e462891e01b9af5fba
SrCroqueta <32432079+SrCroqueta@users.noreply.github.com>
2023-03-01 19:01:05 +0100

    Add files via upload

---
+++ assets/js/cookies-advice.js
@@ -0,0 +1,12 @@
+﻿const botonCookie = document.getElementById("cookies-advice");
+
+if (sessionStorage.getItem("cookies") === null) {
+    botonCookie.style.display = "inline";
+} else {
+    botonCookie.style.display = "none";
+}
+
+botonCookie.addEventListener('click', ()=>{
+    botonCookie.style.display = "none";
+    sessionStorage.setItem("cookies", "aceptadas");
+});
