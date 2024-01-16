a5c9e5c90fdb3e2f4df987dbda2f62a22a05fa56
SrCroqueta <32432079+SrCroqueta@users.noreply.github.com>
2024-01-16 03:33:18 +0100

    Add files via upload

---
+++ assets/js/year-autoupdate.js
@@ -0,0 +1,12 @@
+﻿// Actualizar año actual en el footer
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtiene el elemento año por su ID
+    var yearElement = document.getElementById("copyr-list");
+
+    // Obtiene el año actual
+    var currentYear = new Date().getFullYear();
+
+    // Actualiza el contenido del elemento con el año actual
+    yearElement.innerHTML = "©&nbsp;&nbsp;1996 - " + currentYear +".&nbsp;&nbsp;&nbsp;─&nbsp;&nbsp;&nbsp;Asociación Acéptalos.";
+});
