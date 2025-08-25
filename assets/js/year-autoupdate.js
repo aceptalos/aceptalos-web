0d8f2b0d872691b015c7fa6e98fe8d015b41f612
JotaQC <32432079+JotaQC@users.noreply.github.com>
2025-08-25 10:23:28 +0200

    Update year-autoupdate.js

--- assets/js/year-autoupdate.js
+++ assets/js/year-autoupdate.js
@@ -1,13 +1,6 @@
 ﻿// Actualizar año actual en el footer
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtiene el elemento año por su ID
-    var yearElement = document.getElementById("copyr-list");
-
-    // Obtiene el año actual
-    var currentYear = new Date().getFullYear();
-
-    // Actualiza el contenido del elemento con el año actual
-    yearElement.innerHTML = `&copy;&ensp;1996&ndash;${currentYear}&emsp;Asociación Acéptalos`;
+    const currentYear = new Date().getFullYear();
+    document.getElementById("year-range").textContent = `1996–${currentYear}`;
 });
-
